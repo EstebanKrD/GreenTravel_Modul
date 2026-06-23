3ff3a3366ad98bc5e53ead990ac2e395430106d4
Esteban Cardona <krdonaj@gmail.com>
2026-06-22 21:35:00 -0500

    feat: implement jwt service methods

--- Crud/src/main/java/Greentrvel_Modul/Crud/security/JwtService.java
+++ Crud/src/main/java/Greentrvel_Modul/Crud/security/JwtService.java
@@ -1,21 +1,62 @@
 package Greentrvel_Modul.Crud.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
+
 import javax.crypto.SecretKey;
+import java.util.Date;
 
 @Service
 public class JwtService {
 
-    private  final SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
     private final long jwtExpiration = 900000; // esto equivale a 15 minutos que dura el token una vez generado
 
     public String generarToken(String username) {
+        Date issuedAt = new Date();
 
+        Date expirationDate = new Date(System.currentTimeMillis() + jwtExpiration);
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(issuedAt)
+                .setExpiration(expirationDate)
+                .signWith(key)
+                .compact();
     }
 
+    public String obtenerUsername(String token) {
+
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public boolean validarToken(String token) {
+
+        try {
+
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+
+            return true;
+
+        } catch (Exception e) {
+
+            return false;
+
+        }
+    }
 
 }
