a9f660a835d281509c00dd33a30e1f7f26f6f549
Luza Montiel <luzamontiel20@gmail.com>
2026-06-24 23:25:18 -0500

    feat(export): configure pdf and csv export libraries

--- Crud/src/main/java/Greentrvel_Modul/Crud/service/CsvReportService.java
+++ Crud/src/main/java/Greentrvel_Modul/Crud/service/CsvReportService.java
@@ -1,4 +1,14 @@
 package Greentrvel_Modul.Crud.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CsvReportService {
+
+    private final DashboardService dashboardService;
+
+    public CsvReportService(DashboardService dashboardService) {
+        this.dashboardService = dashboardService;
+    }
+
 }
--- Crud/src/main/java/Greentrvel_Modul/Crud/service/PdfReportService.java
+++ Crud/src/main/java/Greentrvel_Modul/Crud/service/PdfReportService.java
@@ -1,6 +1,14 @@
 package Greentrvel_Modul.Crud.service;
 
+import org.springframework.stereotype.Service;
 
+@Service
 public class PdfReportService {
+
+    private final DashboardService dashboardService;
+
+    public PdfReportService(DashboardService dashboardService) {
+        this.dashboardService = dashboardService;
+    }
 
 }
